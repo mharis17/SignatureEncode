91295cc6f81bdba0580f439cf7060010213587ca
Jibran Ahmed <ahmedjibran73@gmail.com>
2023-05-15 15:21:01 +0500

    forth commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Connection from "./components/connection/connection";
 import Home from "./components/connection/home";
+import Reciever from "./components/connection/reciever";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Connection />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/reciever" element={<Reciever />} />
       </Routes>
     </Router>
   );
