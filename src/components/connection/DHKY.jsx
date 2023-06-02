eeff4a988273752bae0a4ef3cc36e3eadd59ae6f
Jibran Ahmed <ahmedjibran73@gmail.com>
2023-06-02 15:43:50 +0500

    Final

--- src/components/connection/DHKY.jsx
+++ src/components/connection/DHKY.jsx
@@ -177,7 +177,17 @@ function Dhky() {
           <div className="mt-2">
             <input
               // formControlName="rAddress"
+              placeholder="A or B"
               onChange={(e) => setA_B(e.target.value)}
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+          <div className="mt-2">
+            <input
+              // formControlName="rAddress"
+              placeholder="a or b"
+              onChange={(e) => setab(e.target.value)}
               required
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
